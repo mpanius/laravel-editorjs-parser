a2154d892e0cf7e6db3aa3518491de2dbd0e9eb2
Mike <1970673+mpanius@users.noreply.github.com>
2023-05-13 13:47:11 +0300

    Add amprender

--- src/LaravelEditorJs.php
+++ src/LaravelEditorJs.php
@@ -43,4 +43,32 @@ class LaravelEditorJs
             throw new \Exception($e->getMessage());
         }
     }
+    public function render_amp(string $data) : string
+    {
+        try {
+            $configJson = json_encode(config('laravel_editorjs.config') ?: []);
+
+            $editor = new EditorJS($data, $configJson);
+
+            $renderedBlocks = [];
+
+            foreach ($editor->getBlocks() as $block) {
+
+                $viewName = "laravel_editorjs::ampblocks." . Str::snake($block['type'], '-');
+
+                if (! View::exists($viewName)) {
+                    $viewName = 'laravel_editorjs::blocks.not-found';
+                }
+
+                $renderedBlocks[] = View::make($viewName, [
+                    'type' => $block['type'],
+                    'data' => $block['data']
+                ])->render();
+            }
+
+            return implode($renderedBlocks);
+        } catch (EditorJSException $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
 }
