37702093a48b3d40d5b5281fd6a682a95fe24686
Mike <1970673+mpanius@users.noreply.github.com>
2023-07-30 15:00:42 +0300

    Update LaravelEditorJs.php

--- src/LaravelEditorJs.php
+++ src/LaravelEditorJs.php
@@ -15,7 +15,7 @@ class LaravelEditorJs
      * @param string $data
      * @return string
      */
-    public function render(string $data) : string
+    public function render(string $data,$template_dir = 'html') : string
     {
         try {
             $configJson = json_encode(config('laravel_editorjs.config') ?: []);
@@ -26,10 +26,10 @@ class LaravelEditorJs
 
             foreach ($editor->getBlocks() as $block) {
 
-                $viewName = "laravel_editorjs::blocks." . Str::snake($block['type'], '-');
+                $viewName = "laravel_editorjs::{$template_dir}." . Str::snake($block['type'], '-');
 
                 if (! View::exists($viewName)) {
-                    $viewName = 'laravel_editorjs::blocks.not-found';
+                    $viewName = "laravel_editorjs::{$template_dir}.not-found";
                 }
 
                 $renderedBlocks[] = View::make($viewName, [
@@ -43,60 +43,5 @@ class LaravelEditorJs
             throw new \Exception($e->getMessage());
         }
     }
-    public function render_amp(string $data) : string
-    {
-        try {
-            $configJson = json_encode(config('laravel_editorjs.config') ?: []);
-
-            $editor = new EditorJS($data, $configJson);
-
-            $renderedBlocks = [];
-
-            foreach ($editor->getBlocks() as $block) {
-
-                $viewName = "laravel_editorjs::ampblocks." . Str::snake($block['type'], '-');
-
-                if (! View::exists($viewName)) {
-                    $viewName = 'laravel_editorjs::blocks.not-found';
-                }
-
-                $renderedBlocks[] = View::make($viewName, [
-                    'type' => $block['type'],
-                    'data' => $block['data']
-                ])->render();
-            }
-
-            return implode($renderedBlocks);
-        } catch (EditorJSException $e) {
-            throw new \Exception($e->getMessage());
-        }
-    }
-    public function render_turbo(string $data) : string
-    {
-        try {
-            $configJson = json_encode(config('laravel_editorjs.config') ?: []);
-
-            $editor = new EditorJS($data, $configJson);
-
-            $renderedBlocks = [];
-
-            foreach ($editor->getBlocks() as $block) {
-
-                $viewName = "laravel_editorjs::turboblocks." . Str::snake($block['type'], '-');
-
-                if (! View::exists($viewName)) {
-                    $viewName = 'laravel_editorjs::blocks.not-found';
-                }
-
-                $renderedBlocks[] = View::make($viewName, [
-                    'type' => $block['type'],
-                    'data' => $block['data']
-                ])->render();
-            }
-
-            return implode($renderedBlocks);
-        } catch (EditorJSException $e) {
-            throw new \Exception($e->getMessage());
-        }
-    }
+    
 }
