392fc34d665e407200ebe68124aa8555cea2136e
Mikhail Panyushkin <mpanius@gmail.com>
2023-10-21 19:37:19 +0300

    fix img template

--- resources/views/default/image.blade.php
+++ resources/views/default/image.blade.php
@@ -2,9 +2,11 @@
 @isset($data['file']['url'])
     <figure class="editor-js-image-figure">
         <img class="editor-js-image" src="{{$data['file']['url']}}"/>
-        <figcaption class="editor-js-image-caption">@isset($data['link'])
+
+        <figcaption class="editor-js-image-caption">
+            @isset($data['link'])
                 <a class="editor-js-image-caption-link" href="{{$data['link']}}">
-                    {!!  $data['alt'] ? __('Изображение').' '.$data['alt']: __('Источник изображения')  !!}}
+                    {!!  $data['alt'] ? __('Изображение').' '.$data['alt'] : __('Источник изображения')  !!}
                 </a>
             @else
                 {!! $data['alt'] ? __('Изображение').' '.$data['alt']: __('Источник изображения') !!}
