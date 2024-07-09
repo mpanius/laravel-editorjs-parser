0cd48033499d15dd5e15a5bf2117b4063f0ae214
Mikhail Panyushkin <mpanius@gmail.com>
2024-07-10 01:23:07 +0300

    Fix amp

--- resources/views/amp/embed.blade.php
+++ resources/views/amp/embed.blade.php
@@ -1,19 +1,27 @@
 <div class="editor-js-embed">
 
-    @if($data['service'] == 'youtube')
+    @switch($data['service'])
+        @case('youtube')
         <amp-youtube data-videoid="{{str_replace('https://www.youtube.com/embed/','',Str::before($data['embed'],'?'))}}"
         layout="responsive"
         width="480"
         height="270">
         </amp-youtube>
-    @else
-        <amp-iframe   width="480"
-                      height="270"
-                      sandbox="allow-scripts allow-same-origin"
-                      layout="responsive"
-                      allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
-                      frameborder="0" class="aspect-w-16 aspect-h-9 w-full h-64" src="{{ $data['embed'] }}"></amp-iframe>
-    @endif
+        @break
+        @case('vkontakte')
+            <amp-iframe src="{!! str_replace('&amp;','&',$data['embed']) !!}" width="853" height="480"
+                    allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;" frameborder="0"
+                    allowfullscreen></amp-iframe>
+            @break
+        @case('rutube')
+            <amp-iframe src="{!! str_replace('&amp;','&',$data['embed']) !!}" width="720" height="405"
+                    frameBorder="0" allow="clipboard-write; autoplay" allowFullScreen></amp-iframe>
+
+            @break
+        @default
+            <amp-iframe class="aspect-w-16 aspect-h-9 w-full h-64"  frameborder="0" allowfullscreen="" src="{!! $data['embed'] !!}"></amp-iframe>
+    @endswitch
+
     <div class="prose prose-2xl my-2 mx-auto text-center">
         {{ $data['caption'] ?? '' }}
     </div>
