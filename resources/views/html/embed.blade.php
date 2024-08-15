51ed77c7bb5f35e2666ca861ca4eabe2faf7a12b
Mikhail Panyushkin <mpanius@gmail.com>
2024-08-15 21:37:25 +0300

    add new advertising type

--- resources/views/html/embed.blade.php
+++ resources/views/html/embed.blade.php
@@ -1,7 +1,6 @@
 <div class="editor-js-embed relative">
 
 
-
     @switch($data['service'])
         @case('youtube')
             <script async src="https://cdn.viqeo.tv/js/vq_starter.js"></script>
@@ -12,25 +11,32 @@
                 data-profile="17226"
                 data-videoSrc="{{str_replace('&amp;','&',$data['embed'])}}"
             >
-                <iframe src="https://cdn.viqeo.tv/embed/?videoSrc={{str_replace('&amp;','&',$data['embed'])}}&playerId=4931" width="100%" height="100%" style="position:absolute;" frameBorder="0" allowFullScreen></iframe>
+                <iframe
+                    src="https://cdn.viqeo.tv/embed/?videoSrc={{str_replace('&amp;','&',$data['embed'])}}&playerId=4931"
+                    width="100%" height="100%" style="position:absolute;" frameBorder="0" allowFullScreen></iframe>
             </div>
             @break
         @case('vkontakte')
-            <iframe class="w-full aspect-video" src="{!! str_replace('&amp;','&',$data['embed']) !!}" width="853" height="480"
+            <iframe class="w-full aspect-video" src="{!! str_replace('&amp;','&',$data['embed']) !!}" width="853"
+                    height="480"
                     allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;" frameborder="0"
                     allowfullscreen></iframe>
             @break
         @case('rutube')
-            <iframe class="w-full aspect-video" src="{!! str_replace('&amp;','&',$data['embed']) !!}" width="720" height="405"
+            <iframe class="w-full aspect-video" src="{!! str_replace('&amp;','&',$data['embed']) !!}" width="720"
+                    height="405"
                     frameBorder="0" allow="clipboard-write; autoplay" allowFullScreen></iframe>
 
             @break
-            @case('telegram')
+        @case('telegram')
 
-            <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-post="{{str_replace('https://t.me/','',\Illuminate\Support\Str::beforeLast($data['embed'],'?'))}}" data-width="100%"></script>
+            <script async src="https://telegram.org/js/telegram-widget.js?22"
+                    data-telegram-post="{{str_replace('https://t.me/','',\Illuminate\Support\Str::beforeLast($data['embed'],'?'))}}"
+                    data-width="100%"></script>
             @break
         @default
-            <iframe class="aspect-w-16 aspect-h-9 w-full h-64"  frameborder="0" allowfullscreen="" src="{!! $data['embed'] !!}"></iframe>
+            <iframe class="aspect-w-16 aspect-h-9 w-full h-64" frameborder="0" allowfullscreen=""
+                    src="{!! $data['embed'] !!}"></iframe>
     @endswitch
     <div class="prose prose-2xl my-2 mx-auto text-center">
         {{ $data['caption'] ?? '' }}
