6647fb71e68899a3b09f9e561274e0e1355e9e2d
Mikhail Panyushkin <mpanius@gmail.com>
2025-08-02 15:06:55 +0300

    add render array

--- src/LaravelEditorJsParser.php
+++ src/LaravelEditorJsParser.php
@@ -58,4 +58,46 @@ class LaravelEditorJsParser
             return implode($renderedBlocks);
 
     }
+
+    public function renderBlocks(string $data,$template_dir = 'default', ?array $media = null) : array
+    {
+
+
+
+
+        $configJson = json_encode(config('laravel-editorjs-parser.config') ?: []);
+
+        $editor = new EditorJS($data, $configJson);
+
+        $renderedBlocks = [];
+
+
+
+
+        foreach ($editor->getBlocks() as $block) {
+
+            $viewName = "laravel-editorjs-parser::{$template_dir}." . Str::snake($block['type'], '-');
+
+            if (! View::exists($viewName)) {
+                if($template_dir === 'default')
+                {
+                    $viewName = "laravel-editorjs-parser::default.not-found";
+                } else
+                {
+                    $viewName = "laravel-editorjs-parser::default." . Str::snake($block['type'], '-');
+                    if(!View::exists($viewName)){
+                        $viewName = "laravel-editorjs-parser::default.not-found";
+                    }
+                }
+            }
+
+            $renderedBlocks[] = view($viewName, [
+                'type' => $block['type'],
+                'data' => $block['data']
+            ])->render();
+        }
+
+        return $renderedBlocks;
+
+    }
 }
