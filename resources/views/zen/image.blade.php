2dc9a22985ab6df7bcee372dcd68fbdaa683675f
Mikhail Panyushkin <mpanius@gmail.com>
2025-08-07 19:28:32 +0300

    fix parse light image (add resize)

--- resources/views/zen/image.blade.php
+++ resources/views/zen/image.blade.php
@@ -3,10 +3,10 @@
         $mediaId = $data['file']['media_id'] ?? $data['media_id'] ?? null;
         $originalHeight = null;
         $originalWidth = null;
-     if ($mediaId && ($media = \Illuminate\Support\Facades\Cache::remember('media_'.$mediaId, 1800, fn() =>  \Spatie\MediaLibrary\MediaCollections\Models\Media::find($mediaId)))) {
+     if ($mediaId && ($media = \Spatie\MediaLibrary\MediaCollections\Models\Media::find($mediaId))) {
            $imageUrl = $media->getUrl() ?? null;
-           $originalWidth = $media->width ?? 0;
-           $originalHeight = $media->height ?? 0;
+           $originalWidth = $media->width ?? $media->getCustomProperty('width') ?? 0;
+           $originalHeight = $media->height ?? $media->getCustomProperty('height') ?? 0;
 
        } else {
            $imageUrl = $data['file']['url'] ? normalize($data['file']['url']) : null;
@@ -14,29 +14,30 @@
            $originalHeight = $data['file']['height'] ?? 0;
      }
      $width = 1000;
-         $height = null;
+     $height = null;
      if($originalWidth > 2000) {
          $width = 2000;
          $height = round($originalHeight * 2000 / $originalWidth);
-     }elseif (($originalWidth > 0) && ($originalWidth < 1000)){
-         $width = 1000;
+     }elseif (($originalWidth > 0) && ($originalWidth < 1000) && ($originalHeight > 0)){
+
          $height = round($originalHeight * 1000 / $originalWidth);
      }
 
     @endphp
 
     @if(!empty($imageUrl))
-    <figure>
-        <img width="{{$width}}" @if($height) height="{{$height}}" @endif
-             src="{{$height}}" src="{{ img($imageUrl, $width, $height) }}" @if(!empty($data['caption']))alt="{{ $data['caption'] }}"@endif
-        />
+        <figure>
+            <img width="{{$width}}" @if($height) height="{{$height}}" @endif
+            src="{{$height}}" src="{{ img($imageUrl, $width, $height) }}"
+                 @if(!empty($data['caption']))alt="{{ $data['caption'] }}"@endif
+            />
 
 
-        @if (($data['caption'] ?? null) || ($data['source'] ?? null))
-            <figcaption>
-                <span>{{ $data['caption'] ?? ''}}</span><span>{{ $data['source'] ?? '' }}</span>
-            </figcaption>
-        @endif
-    </figure>
+            @if (($data['caption'] ?? null) || ($data['source'] ?? null))
+                <figcaption>
+                    <span>{{ $data['caption'] ?? ''}}</span><span>{{ $data['source'] ?? '' }}</span>
+                </figcaption>
+            @endif
+        </figure>
     @endif
 </div>
