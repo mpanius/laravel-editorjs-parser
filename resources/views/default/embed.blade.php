d5b507c7a060684d10f1997285f761dea6c674c0
Mikhail Panyushkin <mpanius@gmail.com>
2025-09-05 09:49:11 +0300

    fix embed

--- resources/views/default/embed.blade.php
+++ resources/views/default/embed.blade.php
@@ -53,15 +53,17 @@
                 <svg class="w-12 h-12 mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                           d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 002 2z"></path>
-                    </svg>
-                    <span class="text-sm">Загрузка {{ ucfirst($data['service']) }} видео...</span>
-                </div>
+                </svg>
+                <span class="text-sm">Загрузка {{ ucfirst($data['service']) }} видео...</span>
             </div>
+        </div>
             
-            <!-- Динамический контент -->
-            <div class="embed-dynamic-content" style="display: none;">
-                <!-- Контент создается JS -->
-            </div>
+        <!-- Динамический контент -->
+        <div class="embed-content" style="display: none; position: relative; width: 100%; padding-bottom: 56.25%;">
+            @if($data['service'] === 'youtube')
+                <div class="viqeo-external_player" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></div>
+            @endif
+            <!-- Контент создается JS для других сервисов -->
         </div>
     </div>
 
