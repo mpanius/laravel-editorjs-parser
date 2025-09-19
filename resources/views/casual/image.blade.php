488cbbc4b9cc59048bf1ce301903cd2a7afd0eff
Mikhail Panyushkin <mpanius@gmail.com>
2025-09-19 11:49:01 +0300

    feat(editorjs-casual): добавить шаблон casual/image.blade.php с поддержкой srcset/sizes и lightbox
    - Включает адаптивное поведение (glightbox/resize-image) и корректный srcset/sizes
    - Учитывает максимальные размеры из конфига и подписи/ссылки в figcaption

--- resources/views/casual/image.blade.php
+++ resources/views/casual/image.blade.php
@@ -1,44 +1,78 @@
 @php
 
     $hasSet = true;
+    $isSmall = ($data['originalWidth'] < $data['maxDeskWidth']) || ($data['originalHeight'] < $data['maxDeskHeight']);
 @endphp
-<figure @class(['image-stretched' => $data['stretched'] ?? false, 'image-with-border' => $data['withBorder'], 'image-with-background' => $data['withBackground'] ?? false])>
-    <img
-            loading="lazy"
-            decoding="async"
-            class="content-image dynamic-lightbox"
-            @isset($data['originalWidth'])
-            width="{{$data['originalWidth']}}"
-            height="{{$data['originalHeight']}}"
-            data-original-width="{{$data['originalWidth']}}"
-            data-original-height="{{$data['originalHeight']}}"
-            data-max-width="{{$data['maxDeskWidth']}}"
-            data-max-height="{{$data['maxDeskHeight']}}"
-            data-original-image="{{$data['imageUrl']}}"
-            style="aspect-ratio: {{$data['originalWidth']}} / {{$data['originalHeight']}};"
-            src="{{img($data['imageUrl'],$data['width'],$data['height'],false)}}"
-            srcset="
-            @foreach($data['srcSet'] as $width)
-            @if($hasSet && ($data['originalWidth'] > $width))
-            {{img(url: $data['imageUrl'], width:$width)}} {{$width}}w,
-            @elseif($hasSet)
-              @php $hasSet = false; @endphp
-              {{img(url: $data['imageUrl'], width:$data['originalWidth'])}} {{$data['originalWidth']}}w
-            @else @break  @endif @endforeach"
-            sizes="{{$data['srcSizes']}}"
-            @endisset
+<figure @class(['editor-js-image','image-stretched' => $data['stretched'] ?? false, 'image-with-border' => $data['withBorder'], 'image-with-background' => $data['withBackground'] ?? false])>
+    @if(!$isSmall)
+        <a href="{{img($data['imageUrl'],$data['originalWidth'],$data['originalHeight'],false)}}"
+           class="glightbox _gallery resize-image"
+           data-original-width="{{$data['originalWidth']}}"
+           data-original-height="{{$data['originalHeight']}}"
+           data-download-link="{{$data['imageUrl']}}"
+           @if($data['caption'] ?? null) data-title="{{$data['caption']}}" @endif>
 
-            alt="{{$data['caption'] ?? ''}}"
-    />
-    @if (($data['caption'] ?? null) || ($data['alt'] ?? null) || ($data['link'] ?? null) )
-        <figcaption class="mx-auto text-center ">
-            <span>{{ ($data['caption'] ?? false) ? htmlspecialchars_decode($data['caption']) : ''}}</span>
-            @if($data['link'] ?? null)
-                <div class="text-xs @if(!empty($data['caption'] ?? null))mt-2 @endif"><a target="_blank"
-                                                                                         class="no-underline text-gray-500 hover:text-blue-500"
-                                                                                         href="{{$data['link']}}">
-                        {{ ($data['alt'] ?? false) ? __('Изображение').' '. htmlspecialchars_decode($data['alt']): __('Источник изображения') }}</a></div>
+            <div class="image-container">
+                <img
+                        loading="lazy"
+                        decoding="async"
+                        src="{{img($data['imageUrl'],$data['width'],$data['height'],false)}}"
+                        data-resizewidth="{{$data['width']}}"
+                        data-resizeheight="{{$data['height']}}"
+                        class="block max-w-full h-auto mx-auto"
+                        alt="{{$data['caption'] ?? ''}}"
+
+                        width="{{ $data['width'] }}"
+                        height="{{ $data['originalWidth'] /$data['originalHeight'] }}"
+                        style="aspect-ratio: {{$data['originalWidth']}} / {{$data['originalHeight']}};"
+
+                        srcset="@foreach($data['srcSet'] as $width)
+                         @if($hasSet && ($data['originalWidth'] > $width)) {{img(url: $data['imageUrl'], width:$width)}} {{$width}}w, @elseif($hasSet)
+                            @php $hasSet = false; @endphp
+
+                @else
+                    @break
+                @endif @endforeach"
+                sizes="{{$data['srcSizes']}}"
+                />
+            </div>
+        </a>
+    @else
+        <div>
+            <div class="image-container">
+                <img
+                        loading="lazy"
+                        decoding="async"
+                        src="{{img($data['imageUrl'],$data['width'],$data['height'],false)}}"
+                        width="{{ $data['originalWidth'] }}"
+                        height="{{ $data['originalHeight'] }}"
+                        class="block max-w-full h-auto mx-auto"
+                        alt="{{$data['caption'] ?? ''}}"
+                        style="aspect-ratio: {{$data['originalWidth']}} / {{$data['originalHeight']}};"
+                        @foreach($data['srcSet'] as $width)
+                            @if($hasSet && ($data['originalWidth'] > $width))
+                                {{img(url: $data['imageUrl'], width:$width)}} {{$width}}w,
+                        @elseif($hasSet)
+                            @php $hasSet = false; @endphp
+                            {{img(url: $data['imageUrl'], width:$data['originalWidth'])}} {{$data['originalWidth']}}w
+                @else
+                    @break
+                @endif @endforeach"
+                sizes="{{$data['srcSizes']}}"
+
+                />
+            </div>
             @endif
-        </figcaption>
+            @if (($data['caption'] ?? null) || ($data['alt'] ?? null) || ($data['link'] ?? null) )
+                <figcaption class="mx-auto text-center ">
+                    <span>{{ ($data['caption'] ?? false) ? htmlspecialchars_decode($data['caption']) : ''}}</span>
+                    @if($data['link'] ?? null)
+                        <div class="text-xs @if(!empty($data['caption'] ?? null))mt-2 @endif"><a target="_blank"
+                                                                                                 class="no-underline text-gray-500 hover:text-blue-500"
+                                                                                                 href="{{str_replace('&nbsp;','',$data['link'])}}">
+                                {{ ($data['alt'] ?? false) ? __('Изображение').' '. html_entity_decode($data['alt']): __('Источник изображения') }}</a>
+                        </div>
+                    @endif
+                </figcaption>
     @endif
 </figure>
